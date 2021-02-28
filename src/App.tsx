1cda2bff073d01f6a29b566daded2df22b4d657e
Ieva Kaziukonyte <kaziukonyte.ieva@gmail.com>
2021-02-28 20:12:03 +0100

    firebase auth setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import SignIn from "./components/SignIn";
 import Dashboard from "./components/Dashboard";
 import { UserContext } from "./providers/UserProvider";
 
 const App: React.FC = () => {
   const { authUser, isLoading } = useContext(UserContext);
-
-  useEffect(() => {}, [authUser.id]);
 
   if (isLoading) return <div>Loading...</div>;
   if (!isLoading && !authUser.id) return <SignIn />;
