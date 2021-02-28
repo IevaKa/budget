0f25efc5921eec1e2e83aa2902ad577fa0a9dd5e
Ieva Kaziukonyte <kaziukonyte.ieva@gmail.com>
2021-02-28 21:49:31 +0100

    add email + password signup

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,26 @@ import React, { useContext } from "react";
 import SignIn from "./components/SignIn";
 import Dashboard from "./components/Dashboard";
 import { UserContext } from "./providers/UserProvider";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import * as routes from "./constants/routes";
 
 const App: React.FC = () => {
   const { authUser, isLoading } = useContext(UserContext);
+  const LandingComponent = !isLoading && !authUser.id ? SignIn : Dashboard;
 
   if (isLoading) return <div>Loading...</div>;
-  if (!isLoading && !authUser.id) return <SignIn />;
-  return <Dashboard />;
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact={true}
+          path={routes.LANDING}
+          component={LandingComponent}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,9 +1,45 @@
-import React from "react";
-import { authMethods } from "../firebase";
+import React, { useState } from "react";
+import { auth, db, authMethods } from "../firebase";
 
 const SignIn: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.SyntheticEvent<EventTarget>) => {
+    e.preventDefault();
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      if (user !== null) {
+        db.createUserProfileDocument(user);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div>
+      <form className="SignUp" onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Sign Up" />
+      </form>
       <button onClick={authMethods.signInWithGoogle}>
         Sign in with google
       </button>
